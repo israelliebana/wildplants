f5cc60553f1924a6556a8b15ceb856aa92fdcc84
Israel Liebana de Marcos <theshadu@gmail.com>
2020-04-06 08:01:22 +0200

    Initial commit

---
+++ src/main/java/es/devfromt/wildplants/WildplantsApplication.java
@@ -0,0 +1,13 @@
+package es.devfromt.wildplants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildplantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WildplantsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/devfromt/wildplants/WildplantsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.devfromt.wildplants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildplantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
